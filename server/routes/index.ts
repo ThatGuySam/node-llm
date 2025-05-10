bb1118f4abf2ed8f59b859d5e4fbb0bd84c196e7
ThatGuySam <sam@sam.lc>
2025-05-10 12:47:49 -0500

    add: nitro routes

---
+++ server/routes/index.ts
@@ -0,0 +1,5 @@
+export default defineEventHandler((event) => {
+  // eslint-disable-next-line no-console
+  console.log({ event })
+  return 'Start by editing <code>server/routes/index.ts</code>.'
+})
